90b1e4b559722a112fc4ca784b291f96b8211d0c
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-04-20 22:45:25 +0300

    done

--- src/redux/reducers/appReducer.jsx
+++ src/redux/reducers/appReducer.jsx
@@ -1,19 +1,18 @@
-import { SET_LOADING } from "../types/appTypes";
-import { CLEAR_LOADING } from "../types/appTypes";
+import { SET_LOADING, CLEAR_LOADING } from "../types/appTypes";
 
 const initialState = {
-    loading:false,
-}
+  loading: false,
+};
 
-export default (state = initialState, { type, payload }) => {
+const appReducer = (state = initialState, { type, payload }) => {
   switch (type) {
+    case SET_LOADING:
+      return { ...state, loading: true };
+    case CLEAR_LOADING:
+      return { ...state, loading: false };
 
-  case SET_LOADING:
-    return { ...state, loading:true }
-  case CLEAR_LOADING:
-    return { ...state, loading:false }
-
-  default:
-    return state
+    default:
+      return state;
   }
-}
+};
+export default appReducer;
